446f24ad881c1bd070437f388a7f669300e1f652
Philippe-Henri Gosselin <philippe-henri.gosselin@ensea.fr>
2016-11-02 17:02:46 +0100

    petits bugs

--- dia2code/build.sh
+++ dia2code/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p build  || exit 2
-cd build || exit 2
-cmake .. || exit 2
-make -s -j4 || exit 2
+mkdir -p build  || exit 2
+cd build || exit 2
+cmake .. || exit 2
+make -s -j4 || exit 2
 cd ..
 
--- docker/run_docker_x11.sh
+++ docker/run_docker_x11.sh
@@ -8,13 +8,13 @@ fi
 IMAGE=$1
 
 docker run -it \
-    --user=$USER \
-    --env="$DISPLAY" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --user=$(id -u $USER) \
+    --env="DISPLAY" \
     $IMAGE
     /bin/bash ./bin/run
 
