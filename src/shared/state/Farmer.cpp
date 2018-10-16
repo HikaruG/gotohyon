75add69aa3b1fa2caf42240255fc8bd327b9bd78
ben <benj.hgyon@orangefr>
2018-10-16 11:09:29 +0200

    state dia little modif property and attribute visibility

--- src/shared/state/Farmer.cpp
+++ src/shared/state/Farmer.cpp
@@ -2,3 +2,10 @@
 // Created by ben on 15/10/18.
 //
 #include "Farmer.h"
+
+namespace state{
+    Farmer::Farmer()
+    {
+
+    }
+}
--- src/shared/state/GameObject.cpp
+++ src/shared/state/GameObject.cpp
@@ -2,3 +2,10 @@
 // Created by ben on 15/10/18.
 //
 #include "GameObject.h"
+
+
+using namespace state{
+    GameObject::GameObject(unsigned int gameobject_id) {
+
+    }
+}
