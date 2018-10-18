8407ffcee2d588392154c17f24d9871d98608906
ben <benj.hyon@gmail.com>
2018-10-18 19:40:28 +0200

    type chanign in dia

--- src/shared/state/Terrain.cpp
+++ src/shared/state/Terrain.cpp
@@ -2,3 +2,12 @@
 // Created by ben on 15/10/18.
 //
 #include "Terrain.h"
+
+using namespace state;
+
+Terrain::Terrain (Position pos, int mvt_cost, TerrainType terrain_id)
+{
+    this->terrain_position = pos;
+    this->movement_cost = mvt_cost;
+    this->terrrain_id = terrain_id;
+}
