c9405d818d8c230cb5e8a8bca8f73cf230ef1b1d
ben <benj.hyon@gmail.com>
2018-10-18 19:59:19 +0200

    Unit and building should be ok, dia modified accordingly

--- src/shared/state/Building.cpp
+++ src/shared/state/Building.cpp
@@ -2,3 +2,22 @@
 // Created by ben on 15/10/18.
 //
 #include "Building.h"
+
+using namespace state;
+
+Building::Building (bool can_attack, unsigned int gameobject_id, unsigned int player_id, state::Position pos, state::Property prop, state::BuildingType build_type)
+:GameObject(gameobject_id,player_id,pos, prop)
+{
+    if(prop.get_attack() == 0)
+    {
+        can_attack = false;
+    }
+    else{
+        can_attack = true;
+    }
+    type = build_type;
+}
+
+const BuildingType Building::getBuildingType (){
+    return this->type;
+}
--- src/shared/state/Unit.cpp
+++ src/shared/state/Unit.cpp
@@ -2,3 +2,16 @@
 // Created by ben on 15/10/18.
 //
 #include "Unit.h"
+
+using namespace state;
+
+Unit::Unit (unsigned int movement_range, unsigned int gameobject_id, unsigned int player_id, state::Position pos, state::Property property, UnitType unit_type)
+:GameObject(gameobject_id,player_id,pos, property)
+{
+    this->movement_range = movement_range;
+    this->type = unit_type;
+}
+UnitType const Unit::getUnitType ()
+{
+    return type;
+}
